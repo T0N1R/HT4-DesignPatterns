808852003c6809736f475a97bf0b66d2fa407e69
DESKTOP-9JNVIM8\Antonio <reytono@gmail.com>
2018-02-14 08:08:39 -0600

    Se crea clase Main

---
+++ DesignPatternsHT4/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Universidad del VAlle de Guatemala
+ * Guatemala 14 de febrero 2018
+ * Algoritmos y Estructura de Datos
+ * Hoja de Trabajo 4
+ * El objetivo de esta Hoja de Trabajo es realizar una calculadora para evaluar
+ * expresiones postfix.
+ * @author Antonio #17273
+ * @author Andres Perez #
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
