4b0759f0ab156365cacb150d451877e342a06161
DaronP <per16362@uvg.edu.gt>
2018-02-19 17:06:06 -0600

    Interfaces

--- DesignPatternsHT4/src/HT4/AbstractList.java
+++ DesignPatternsHT4/src/HT4/AbstractList.java
@@ -1,141 +1,20 @@
-
-package HT4;
-
-/**
- *
- * @author Antonio
- */
-
-import java.util.Iterator;
-
-public abstract class AbstractList<E> implements List<E>,Stack<E> {
+public abstract class AbstractList<E>
+       implements List<E>
+{
    public AbstractList()
    // post: does nothing
    {
    }
 
-   @Override
-    public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void addFirst(E value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void addLast(E value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E getFirst() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E getLast() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E removeFirst() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E removeLast() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E remove(E value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void add(E value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E remove() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E get() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean contains(E value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public int indexOf(E value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public int lastIndexOf(E value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E get(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E set(int i, E o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void add(int i, E o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E remove(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void push(E item) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E pop() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E peek() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean empty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-}
+   public boolean isEmpty()
+   // post: returns true iff list has no elements
+   {
+      return size() == 0;
+   }
+  
+  public boolean contains(E value)
+  // pre: value is not null
+  // post: returns true iff list contains an object equal to value
+  {
+	return -1 != indexOf(value);
+  }
--- DesignPatternsHT4/src/HT4/DoublyLinkedNode.java
+++ DesignPatternsHT4/src/HT4/DoublyLinkedNode.java
@@ -1,54 +1,25 @@
+public class DoublyLinkedNode<E>
+{
+protected E data;
+protected DoublyLinkedNode<E> nextElement;
+protected DoublyLinkedNode<E> previousElement;
 
-package HT4;
-
-/**
- *
- * @author Antonio
- */
-public class DoublyLinkedNode<E>{
-    protected E data;
-    protected DoublyLinkedNode<E> nextElement;
-    protected DoublyLinkedNode<E> previousElement;
-
-    public DoublyLinkedNode(E v,DoublyLinkedNode<E> next,DoublyLinkedNode<E> previous){
-        data = v;
-        nextElement = next;
-        if (nextElement != null)
-            nextElement.previousElement = this;
-        previousElement = previous;
-        if (previousElement != null)
-            previousElement.nextElement = this;
-    }
-
-    public DoublyLinkedNode(E v){
-    // post: constructs a single element
-        this(v,null,null);
-    }
-    
-    public DoublyLinkedNode<E> next(){
-    // post: returns reference to next value in list
-        return nextElement;
-    }
-    public DoublyLinkedNode<E> previous(){
-        return previousElement;
-    }
-
-    public void setPrevious(DoublyLinkedNode<E> previous){
-    // post: sets reference to new next value
-    previousElement = previous;
-    }
-
-    public void setNext(DoublyLinkedNode<E> next){
-    // post: sets reference to new next value
+public DoublyLinkedNode(E v,
+                        DoublyLinkedNode<E> next,
+                        DoublyLinkedNode<E> previous)
+{
+    data = v;
     nextElement = next;
-    }
-    public E value(){
-    // post: returns value associated with this element
-    return data;
-    }
-    public void setValue(E value){
-    // post: sets value associated with this element
-    data = value;
-    }
-    
+    if (nextElement != null)
+        nextElement.previousElement = this;
+    previousElement = previous;
+    if (previousElement != null)
+        previousElement.nextElement = this;
+}
+
+public DoublyLinkedNode(E v)
+// post: constructs a single element
+{
+    this(v,null,null);
+}
 }
