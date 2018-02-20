a181b2a582d454073e3d9ba4970b1cbb708dd18a
DESKTOP-9JNVIM8\Antonio <reytono@gmail.com>
2018-02-19 18:33:02 -0600

    modificacion 4 JUnit

---
+++ DesignPatternsHT4/test/HT4/CalculadoraTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HT4;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Antonio
+ */
+public class CalculadoraTest {
+    
+    public CalculadoraTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of calcular method, of class Calculadora.
+     */
+    @Test
+    public void testCalcular() {
+        System.out.println("calcular");
+        String esp = "2 3 + 1 *";
+        Calculadora instance = new Calculadora();
+        int expResult = 5;
+        String result = instance.calcular(esp);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        if(!result.equals(expResult)){
+         fail("The test case is a prototype.");
+        }
+    }
+    
+}
